e1ba7d63691acace94939c936596db57ad3b69ad
Laravel-project <misa20000020@gmail.com>
2020-02-07 10:15:19 +0200

    qwe

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,6 @@
 |
 */
 
-/* Route::get('/', function () {
-    return view('welcome');
-}); */
-Route::get('/', function () {
-    return view('index');
-});
+use App\Http\Controllers\IndexController;
+
+Route::get('/', 'IndexController@index');
